ce7038e26e8df788641af90b02324f4ce909356d
Branden Soropia <b.soropia@gmail.com>
2017-12-02 17:48:17 -0500

    Get marketplace items from backend and render them. Does not update player inventory.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,7 +13,7 @@ router.get('/get_marketplace_items', function (req, res, next) {
     // Get document, or throw exception on error
     try {
         const marketplaceItems = yaml.safeLoad(fs.readFileSync('public/resources/marketplace_items.yaml', 'utf8'));
-        res.send(marketplaceItems)
+        res.send(marketplaceItems);
     } catch (e) {
         console.log(e);
         res.send({ message: "Couldn't get marketplace items. Sorry!"})
--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,12 +1,62 @@
 import React, {Component} from 'react';
+import 'whatwg-fetch';
 
 class Marketplace extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        }
+    }
     /**
-     * Asynchronously set the increment amount.
+     * Populate marketplace with data received from backend.
+     */
+    componentDidMount() {
+        this.setMarketplaceItems();
+    }
+
+    /**
+     * Save marketplace items in state.
+     */
+    setMarketplaceItems = () => {
+        this.getMarketplaceItems()
+            .then(items => { // Save items to state asynchronously
+                this.setState(() => {
+                    return { items }
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    /**
+     * Return promise to get marketplace items from server.
+     */
+    getMarketplaceItems = () => {
+        return fetch('http://localhost:3001/get_marketplace_items')
+            .then(res => res.json())
+            .then(json => {
+                if (json.message) { // Report error if any
+                    console.log(json.message);
+                    return;
+                }
+
+                return json.marketplace_items;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    };
+
+    /**
+     * Iff the user has enough funds, subtract from player funds, update player's inventory and apply score effects.
+     * Otherwise do nothing.
      */
     buyItem = (itemCost, itemName, itemPerSecondIncrease) => {
         // Check if there is enough in score. Subtract, increase amount owned and apply score increase iff true. Otherwise reject
-        if (!this.hasEnoughToPurchase(itemCost)) {// Do nothing if can't afford purchase
+        if (!this.hasEnoughToPurchase(itemCost)) { // Do nothing if can't afford purchase
             console.log(`Sorry you can't afford purchasing ${itemName}!`);
             return;
         }
@@ -16,7 +66,7 @@ class Marketplace extends Component {
     };
 
     /**
-     * Return true iff score is greater than price
+     * Return true iff score is greater than price.
      * @param price
      * @returns {boolean}
      */
@@ -25,12 +75,25 @@ class Marketplace extends Component {
     };
 
     render() {
+        const { items } = this.state;
+
+        const marketplaceItemElements = Object.keys(items).map((itemName, idx) => {
+            const { base_cost , per_second_score_amplifier } = items[itemName];
+
+            return (
+                <span className={'item'} key={`item-${idx}`}>
+                    {
+                        // TODO: apply modifier to cost
+                        `Increase increment by ${per_second_score_amplifier}!`
+                    }
+                    <button className={'buy-button'} key={`buy-button-${idx}`} onClick={() => this.buyItem(base_cost, itemName, per_second_score_amplifier)}>Buy</button>
+                </span>
+            )
+        });
+
         return (
             <div className={'marketplace'}>
-                <span className={'item'}>
-                    Increment by 2!
-                    <button className={'buy-button'} onClick={() => this.buyItem(5, 'item', 2)}>Buy</button>
-                </span>
+                {marketplaceItemElements}
             </div>
         )
     }
